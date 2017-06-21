7f8067b65322801d543c1f4a51ebe219c52919f2
Vladimir Dimitrieski <dimitrieski89@gmail.com>
2017-06-21 22:40:40 +0200

    first project commit

---
+++ my_first_project/src/main/java/com/iktpreobuka/myfirstproject/MyFirstProjectApplication.java
@@ -0,0 +1,12 @@
+package com.iktpreobuka.myfirstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstProjectApplication.class, args);
+	}
+}
