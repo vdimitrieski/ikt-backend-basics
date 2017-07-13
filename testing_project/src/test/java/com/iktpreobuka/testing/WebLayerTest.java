2212686c47ef0c58ad3b5d3b276cd05bf9a8a086
vdimitrieski <dimitrieski89@gmail.com>
2017-07-13 12:36:57 +0200

    improved failing test case

--- testing_project/src/test/java/com/iktpreobuka/testing/WebLayerTest.java
+++ testing_project/src/test/java/com/iktpreobuka/testing/WebLayerTest.java
@@ -13,8 +13,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.iktpreobuka.testing.controllers.HomeController;
+
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest(HomeController.class)
 public class WebLayerTest {
 
     @Autowired
