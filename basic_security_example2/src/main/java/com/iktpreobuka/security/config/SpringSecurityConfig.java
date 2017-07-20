3c8944d6ed834f1348322a71cbbe9372739df52f
milancelikovic <milancel@uns.ac.rs>
2017-07-20 14:00:39 +0200

    basic auth improverd

--- basic_security_example2/src/main/java/com/iktpreobuka/security/config/SpringSecurityConfig.java
+++ basic_security_example2/src/main/java/com/iktpreobuka/security/config/SpringSecurityConfig.java
@@ -3,18 +3,18 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
-
+	
 	@Autowired
 	private AuthenticationEntryPoint authEntryPoint;
 	
@@ -42,8 +42,14 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 		jdbcAuthentication()
 			.usersByUsernameQuery(usersQuery)
 			.authoritiesByUsernameQuery(rolesQuery)
-
+			.passwordEncoder(passwordEncoder())
 			.dataSource(dataSource);
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder(){
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder;
 	}
 
 }
