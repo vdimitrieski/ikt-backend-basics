db85a3c9e796fa862bc76aa81e5de07e06dba308
Vladimir Dimitrieski <dimitrieski89@gmail.com>
2022-11-30 16:19:44 +0100

    New versions of all projects
    Updated for Spring Boot 2.9.6

--- data_access_project/src/main/java/com/iktpreobuka/dataaccess/repositories/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.iktpreobuka.dataaccess.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.iktpreobuka.dataaccess.entities.AddressEntity;
-
-public interface AddressRepository extends CrudRepository<AddressEntity, Integer> {
-
-}
--- tour_of_heroes/src/main/java/com/iktpreobuka/tourofheroes/repositories/HeroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.iktpreobuka.tourofheroes.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.iktpreobuka.tourofheroes.entities.HeroEntity;
-
-public interface HeroRepository extends CrudRepository<HeroEntity, Integer>{
-
-}
