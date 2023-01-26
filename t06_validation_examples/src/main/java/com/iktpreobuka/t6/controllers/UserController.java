e7714d84d469ef6b0158573bf3d8fe5cbcdf1a87
Vladimir Dimitrieski <dimitrieski89@gmail.com>
2023-01-26 11:46:32 +0100

    added global exception handler

--- t06_validation_examples/src/main/java/com/iktpreobuka/t6/controllers/UserController.java
+++ t06_validation_examples/src/main/java/com/iktpreobuka/t6/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.iktpreobuka.t6.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
@@ -8,12 +10,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.iktpreobuka.t6.entities.UserEntity;
@@ -35,11 +41,30 @@ public class UserController {
 		binder.addValidators(userCustomValidator);
 	}
 
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+		Map<String, String> errors = new HashMap<>();
+		ex.getBindingResult().getAllErrors().forEach((error) -> {
+			String fieldName = "";
+			String errorMessage = "";
+			if (error instanceof FieldError) {
+				fieldName = ((FieldError) error).getField();
+				errorMessage = error.getDefaultMessage();
+			} else if (error instanceof ObjectError) {
+				fieldName = ((ObjectError) error).getObjectName();
+				errorMessage = error.getDefaultMessage();
+			}
+			errors.put(fieldName, errorMessage);
+		});
+		return errors;
+	}
+
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<?> saveUser(@Valid @RequestBody UserEntity newUser, BindingResult result) {
-		if (result.hasErrors()) {
-			return new ResponseEntity<>(createErrorMessage(result), HttpStatus.BAD_REQUEST);
-		}
+	public ResponseEntity<?> saveUser(@Valid @RequestBody UserEntity newUser) { // BindingResult result
+//		if (result.hasErrors()) {
+//			return new ResponseEntity<>(createErrorMessage(result), HttpStatus.BAD_REQUEST);
+//		}
 
 		// store user in DB
 		userRepository.save(newUser);
